2143deb0b264b32f4a884c881187bfe0a4a990b8
raquel_e <raqescar@gmail.com>
2022-02-23 00:40:01 -0700

    Fetch repo data and display on site

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //profile information
 const overview = document.querySelector(".overview");
 const username = "raqescar";
+const repoList = document.querySelector(".repo-list");
 
 //fetch information from GitHub profile
 const profileInfo = async function () {
@@ -25,6 +26,25 @@ const displayUserInfo = function (gitData) {
     <p><strong>Location:</strong> ${gitData.location}</p>
     <p><strong>Number of public repos:</strong> ${gitData.public_repos}</p>
   </div> 
-  `
+  `;
   overview.append(userInfo);
+  repoInfo();
 };
+
+//fetch repos
+const repoInfo = async function () {
+    const fetchRepo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepo.json();
+    console.log(repoData);
+    displayRepos(repoData);
+};
+//repoInfo();
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
+  };
